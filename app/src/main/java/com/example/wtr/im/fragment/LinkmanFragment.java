9e7f354499e78fd2a7f24b63b90cc1c6f6e55db0
wtr <396612789@qq.com>
2017-12-19 23:15:06 +0800

    修改创建群

--- app/src/main/java/com/example/wtr/im/fragment/LinkmanFragment.java
+++ app/src/main/java/com/example/wtr/im/fragment/LinkmanFragment.java
@@ -51,7 +51,7 @@ public class LinkmanFragment extends Fragment{
 
     private ListView myListView;
 
-    final private DisplayImageOptions options = getSimpleOptions();
+    private DisplayImageOptions options;
     final private ImageLoader imageLoader = ImageLoader.getInstance();
 
     private ItemAdapter adapter;
@@ -341,6 +341,7 @@ public class LinkmanFragment extends Fragment{
                     });
                     break;
                 case AFRIEND:
+                    options = getPeopleOptions();
                     holder.name.setText(peopleItemList.get(position-4).getName());
                     String sex = peopleItemList.get(position-4).getSex();
                     if(("female").equals(sex))
@@ -353,6 +354,7 @@ public class LinkmanFragment extends Fragment{
                             ,holder.portrait, options,animateFirstListener);
                     break;
                 case AGROUP:
+                    options = getGroupOptions();
                     holder.name.setText(groupItemList.get(position-5-
                             PeopleNum).getName());
                     holder.info.setText(groupItemList.get(position-5-
@@ -369,11 +371,25 @@ public class LinkmanFragment extends Fragment{
     }
 
 
-    private DisplayImageOptions getSimpleOptions() {
+    private DisplayImageOptions getPeopleOptions() {
         DisplayImageOptions options = new DisplayImageOptions.Builder()
                 .showImageOnLoading(R.drawable.default_portrait) //设置图片在下载期间显示的图片
                 .showImageForEmptyUri(R.drawable.default_portrait)//设置图片Uri为空或是错误的时候显示的图片
                 .showImageOnFail(R.drawable.default_portrait)  //设置图片加载/解码过程中错误时候显示的图片
+                .cacheInMemory(true)//设置下载的图片是否缓存在内存中
+                .cacheOnDisk(true)//设置下载的图片是否缓存在SD卡中
+                .imageScaleType(ImageScaleType.IN_SAMPLE_INT)//设置图片以如何的编码方式显示
+                .considerExifParams(true)
+                .bitmapConfig(Bitmap.Config.RGB_565)//设置图片的解码类型
+                .build();//构建完成
+        return options;
+    }
+
+    private DisplayImageOptions getGroupOptions() {
+        DisplayImageOptions options = new DisplayImageOptions.Builder()
+                .showImageOnLoading(R.drawable.group_portrait) //设置图片在下载期间显示的图片
+                .showImageForEmptyUri(R.drawable.group_portrait)//设置图片Uri为空或是错误的时候显示的图片
+                .showImageOnFail(R.drawable.group_portrait)  //设置图片加载/解码过程中错误时候显示的图片
                 .cacheInMemory(true)//设置下载的图片是否缓存在内存中
                 .cacheOnDisk(true)//设置下载的图片是否缓存在SD卡中
                 .imageScaleType(ImageScaleType.IN_SAMPLE_INT)//设置图片以如何的编码方式显示
